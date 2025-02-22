37f3e357808b194c430e146401dd0781637f6c18
CleS <feelthefonk@gmail.com>
2025-02-22 15:46:15 +0100

    adding demo - 1.0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 setup(
     name="barre",
-    version="1.0.4",
+    version="1.0.5",
     py_modules=["barre"],
     description="Minimal progress bar",
     long_description=open('README.md').read(),
