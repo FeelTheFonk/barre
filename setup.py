b445539edd7cda4f2c8aef31b2dff86a17b9ede8
CleS <feelthefonk@gmail.com>
2025-02-22 17:00:14 +0100

    minor fixes, versioning

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name="barre",
-    version="1.0.12",
+    version="1.0.13",
     py_modules=["barre"],
     description="Minimal progress bar",
     long_description=open('README.md').read(),
