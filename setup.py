8f746b4778279367cd09c2afde637474d462c11f
CleS <feelthefonk@gmail.com>
2025-02-22 16:15:16 +0100

    workflow correction #002

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 setup(
     name="barre",
-    version="1.0.7",
+    version="1.0.8",
     py_modules=["barre"],
     description="Minimal progress bar",
     long_description=open('README.md').read(),
